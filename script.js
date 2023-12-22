99ebac9ecc827cac2614ead4179f798ca04fc704
Jonathan Scott <Jscott1421@icloud.com>
2023-12-22 16:33:18 -0500

    added javascript button user interface and scrapped the input functionality of the game.

--- script.js
+++ script.js
@@ -8,36 +8,36 @@
 // COMPLETE ----- 6. Champion a winner!
 
 
-const choices = ['ROCK', 'PAPER', 'SCISSORS']; 
 
+
+const CHOICES = ['ROCK', 'PAPER', 'SCISSORS']; 
 
 
 let playerScore = 0;
 let compScore = 0;
 
 
-// console.log(`Player chooses: ${playerChoice}`);
-// console.log(`Computer chooses: ${compChoice}`);
+const playerScoreNumber = document.querySelector('.player-score-number');
+playerScoreNumber.textContent = playerScore;
+
+const computerScoreNumber = document.querySelector('.computer-score-number');
+computerScoreNumber.textContent = compScore;
+
+
+// Function that plays through one round of ROCK, PAPER, SCISSORS.
 
 const playRound = (computerSelection, playerSelection) => {
-    
-    let getCompChoice = (min, max) => Math.floor(Math.random() * (max -min) + min);
-
-    let getPlayerchoice = (prompt('Choose ROCK, PAPER, or SCISSORS: ')).toUpperCase();
-
-    computerSelection = choices[getCompChoice(0, 3)];
-
-    playerSelection = getPlayerchoice;
-
 
     if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
         playerScore++;
+        playerScoreNumber.textContent = playerScore;
         console.log(`${playerSelection} beats ${computerSelection}! You win this round!`);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
 
     } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER'){
         compScore++
+        computerScoreNumber.textContent = compScore;
         console.log(`${computerSelection} beats ${playerSelection}! LOSER!!! `);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
@@ -45,12 +45,13 @@ const playRound = (computerSelection, playerSelection) => {
     } else if (playerSelection === 'ROCK' && computerSelection === 'ROCK'){
         console.log(`${playerSelection} and ${computerSelection} tie! No one wins! Repeat this round!`);
         console.log(`Your score: ${playerScore}`);
-        console.log(`Computer score: ${compScore}`);
+        console.log(`Computer score: ${compScore}`); 
 
     
     } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK'){
         winner = 'Player';
         playerScore++
+        playerScoreNumber.textContent = playerScore;
         console.log(`${playerSelection} beats ${computerSelection}! You win this round!`);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
@@ -58,6 +59,7 @@ const playRound = (computerSelection, playerSelection) => {
     } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS'){
         winner = 'Computer';
         compScore++;
+        computerScoreNumber.textContent = compScore;
         console.log(`${computerSelection} beats ${playerSelection}! LOSER!!! `);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
@@ -69,12 +71,14 @@ const playRound = (computerSelection, playerSelection) => {
 
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
         playerScore++;
+        playerScoreNumber.textContent = playerScore;
         console.log(`${playerSelection} beats ${computerSelection}! You win this round!`);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
 
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK'){
         compScore++;
+        computerScoreNumber.textContent = compScore;
         console.log(`${computerSelection} beats ${playerSelection}! LOSER!!! `);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
@@ -84,24 +88,54 @@ const playRound = (computerSelection, playerSelection) => {
         console.log(`${playerSelection} and ${computerSelection} tie! No one wins! Repeat this round!`);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${compScore}`);
-    } else {
-        alert('This is not a valid entry! Please try again!');
     };
-}
+};
+
+
+const selectionGroup = document.querySelector('.game-items');
+selectionGroup.addEventListener('click',(event)=> {
+
+    let playerPick = '';
+
+    let getCompChoice = (min, max) => Math.floor(Math.random() * (max -min) + min); 
+    let computerPick = CHOICES[getCompChoice(0, 3)]; 
+
+    switch (event.target.className){
+        case 'rock-button':
+            playerPick = 'ROCK';
+            playRound(computerPick, playerPick);
+            break;
+        
+        case 'paper-button':
+            playerPick = 'PAPER';
+            playRound(computerPick, playerPick);
+            break;
+
+        case 'scissors-button':
+            playerPick = 'SCISSORS';
+            playRound(computerPick, playerPick);
+            break;
+    };
+});
+
 
 const playGame = () => {
 
     alert('Welcome to ROCK, PAPER, SCISSORS. TRY TO BEAT THE COMPUTER BEST 3 OUT OF 5!')
 
-    while (Math.max(playerScore, compScore) < 3){
-        playRound();
-    };
+    // while (Math.max(playerScore, compScore) < 3){
+    //     playRound();
+    // };
 
-    if (playerScore < compScore){
-        alert(`You lost! Sorry, try again next time!`);
-    } else {
-        alert(`Congrats! You win best 3 out of 5!`);
-    };
+    // if (playerScore < compScore){
+    //     alert(`You lost! Sorry, try again next time!`);
+    // } else {
+    //     alert(`Congrats! You win best 3 out of 5!`);
+    // };
+
+    // console.log('Game Concluded. Refresh to play again!')
 }
 
-console.log(playGame());
+
+// playGame();
+playRound();
